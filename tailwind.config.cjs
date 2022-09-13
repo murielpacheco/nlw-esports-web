a9e8304b8f25159dc30419e7096b56e1862ac05b
Muriel Damazio <murieldps@gmail.com>
2022-09-13 10:16:55 -0300

    chore: homepage css layout done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        nlw_gradient:
+          'linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)',
+        gaming_gradient:
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
   },
   plugins: [],
 }
